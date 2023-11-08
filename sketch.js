5b914017dd0ad12cfca404b389d95334b7296a8e
unknown <gabrieuvinici@gmail.com>
2023-11-08 13:49:16 -0300

    pegando dados

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function comprovante(){
+
+    var pergunta = prompt ("se quiser proseguir com a votaçao digite 'ok'")
+alert ("iremos proseguir "+pergunta)
+
+
+var nome = prompt ("informe seu nome?")
+alert ("prazer em conhecer voce "+nome)
+
+var idade = prompt ("informe sua idade?")
+alert ("esta é a sua idade "+idade)
+
+}
