88c26fcd0793de02e761d58bcc06289d6653545b
unknown <gabrieuvinici@gmail.com>
2023-11-08 14:42:43 -0300

    pegando a data e a hora do comprovante

--- sketch.js
+++ sketch.js
@@ -10,13 +10,24 @@ function gerarComprovante() {
 
     var nome = prompt("Informe seu nome:");
     var idade = prompt("Informe sua idade:");
+    var cidade = prompt("Informe sua cidade:");
+    var estado = prompt("Informe seu estado:");
+    
     var imprimirComprovante = prompt("Para prosseguir com o comprovante, digite 'ok'");
 
     if (imprimirComprovante !== 'ok') {
         alert("Precisamos que você escreva 'ok' para continuar.");
         return;
     } else {
-        var comprovanteText = "Comprovante de votação de " + nome + "\n\n" + "Idade: " + idade + " anos";
+        var dataEmissao = new Date();
+        var dataFormatada = dataEmissao.toLocaleString(); // Obtém a data e hora formatada
+
+        var comprovanteText = "Comprovante de votação de " + nome + "\n\n" + 
+                            "Idade: " + idade + " anos\n" + 
+                            "Cidade: " + cidade + "\n" + 
+                            "Estado: " + estado + "\n" +
+                            "Data de Emissão: " + dataFormatada;
+
         document.getElementById("comprovante").innerText = comprovanteText;
     }
 }
