7857336efb1f5748c8ccdfa3b0ad60d0f39fd3e1
unknown <gabrieuvinici@gmail.com>
2023-11-08 14:36:38 -0300

    style incrementado

--- sketch.js
+++ sketch.js
@@ -1,28 +1,22 @@
-function comprovante() {
-    var pergunta = prompt("Se quiser prosseguir com a votação, digite 'ok'");
+function gerarComprovante() {
+    var pergunta = prompt("Para prosseguir com a votação, digite 'ok'");
 
     if (pergunta !== 'ok') {
         alert("Precisamos que você escreva 'ok' para continuar.");
-        // Retorna o código caso não seja escrito 'ok'
         return;
     } else {
         alert("Ok! Preencha os campos a seguir");
     }
 
-    var nome = prompt("Informe seu nome?");
-    var idade = prompt("Informe sua idade?");
-    var imprimirComprovante = prompt("Se quiser prosseguir com o comprovante, digite 'ok'");
+    var nome = prompt("Informe seu nome:");
+    var idade = prompt("Informe sua idade:");
+    var imprimirComprovante = prompt("Para prosseguir com o comprovante, digite 'ok'");
 
     if (imprimirComprovante !== 'ok') {
         alert("Precisamos que você escreva 'ok' para continuar.");
-        // Retorna o código caso não seja escrito 'ok'
         return;
     } else {
         var comprovanteText = "Comprovante de votação de " + nome + "\n\n" + "Idade: " + idade + " anos";
         document.getElementById("comprovante").innerText = comprovanteText;
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e57d5f75deb6c2eb39714ad451e1249346eef3c1
